02e9340ea9ae8ebcd83b4f0ba5a0de6f6208e2ff
hashishreddy <146133960+hashishreddy@users.noreply.github.com>
2025-05-14 11:05:46 +0530

    Update assistant.py

--- SIH 2024/Converse_app/assistant.py
+++ SIH 2024/Converse_app/assistant.py
@@ -3,4 +3,4 @@ from langchain_groq import ChatGroq
 
 llm=ChatGroq(temperature=0.7,
              model_name="gemma2-9b-it",
-             api_key='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')
+             api_key='your-key')
